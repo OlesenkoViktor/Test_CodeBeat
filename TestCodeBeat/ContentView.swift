3df560165573faa52386a95c33e63f65b2438ef0
OlesenkoViktor <OlesenkoViktor@gmail.com>
2023-08-24 05:55:05 -0400

    Fake commit

--- TestCodeBeat/ContentView.swift
+++ TestCodeBeat/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let token = "dbfvahjsdga7dyan99124_bnfFAS"
+    
+    private var i: Int = -1
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(i)")
         }
         .padding()
     }
