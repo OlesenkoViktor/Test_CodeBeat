ada64a237c5d93a28eee273f2a6f8857a4da1083
OlesenkoViktor <OlesenkoViktor@gmail.com>
2023-08-24 05:38:34 -0400

    Initial setup

---
+++ TestCodeBeat/TestCodeBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCodeBeatApp.swift
+//  TestCodeBeat
+//
+//  Created by Viktor on 2023-08-24.
+//
+
+import SwiftUI
+
+@main
+struct TestCodeBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
